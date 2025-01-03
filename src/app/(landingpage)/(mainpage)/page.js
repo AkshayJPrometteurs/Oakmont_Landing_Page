51f32dab072320fe9c53bd5c02bef531a0a21b1f
Akshay Jadhav <akshay.j@prometteurs.in>
2025-01-03 15:48:51 +0530

    Main Page Wrapped

--- src/app/(landingpage)/(mainpage)/page.js
+++ src/app/(landingpage)/(mainpage)/page.js
@@ -1,33 +1,4 @@
-import React from 'react'
-import AITips from '@/components/sections/AITips'
-import BannerContents from '@/components/sections/BannerContents'
-import BecomeAMVPMember from '@/components/sections/BecomeAMVPMember'
-import DownloadApp from '@/components/sections/DownloadApp'
-import SupportedBookmarks from '@/components/sections/SupportedBookmarks'
-import Tournaments from '@/components/sections/Tournaments'
-import MainPageLayout from './layout'
-import WhatsIncluded from '@/components/sections/WhatsIncluded'
-import ReferFriendsAndEarn from '@/components/sections/ReferFriendsAndEarn'
-import FrequentlyAskQuestions from '@/components/sections/FrequentlyAskQuestions'
-import Testimonials from '@/components/sections/Testimonials'
-
-const page = () => {
-    return (
-        <MainPageLayout>
-            <section>
-                <BannerContents/>
-                <DownloadApp/>
-                <AITips/>
-                <SupportedBookmarks/>
-                <Tournaments/>
-                <BecomeAMVPMember href={'/manage-subscription'}/>
-                <WhatsIncluded/>
-                <Testimonials/>
-                <ReferFriendsAndEarn/>
-                <FrequentlyAskQuestions/>
-            </section>
-        </MainPageLayout>
-    )
-}
-
+import React from 'react';
+import MainPageWrapped from './MainPageWrapped';
+const page = () => <MainPageWrapped/>
 export default page
