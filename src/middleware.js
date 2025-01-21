86a242bfb066dabbfef87fa4114c651e49a3f7c9
Akshay Jadhav <akshay.j@prometteurs.in>
2025-01-21 13:58:20 +0530

    Some Changes

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,10 @@
-import Cookies from 'js-cookie';
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
     const path = request.nextUrl.pathname;
     const publicPaths = ['/login', '/signup', '/forget-password', '/reset-password'];
     const isPublicPath = publicPaths.some((publicPath) => path.startsWith(publicPath));
-    const token = Cookies.get('_om_at');
+    const token = request.cookies.get('_om_at');
     if (isPublicPath && token) { return NextResponse.redirect(new URL('/', request.url)); }
     if (!isPublicPath && !token) { return NextResponse.redirect(new URL('/login', request.url)); }
     return NextResponse.next();
