e586a9c55c0a5954d5bfda9c0fedba0d865cd888
Akshay Jadhav <akshay.j@prometteurs.in>
2025-04-02 12:29:04 +0530

    Privacy policy page added

---
+++ src/app/privacy-policy/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PrivacyPolicy from "@/components/pages/PrivacyPolicy";
+
+export const metadata = {
+    title: "Oakmont Athletic : Privacy Policy",
+    description: "Oakmont Athletic : Privacy Policy"
+};
+
+const page = () => <PrivacyPolicy/>
+
+export default page
